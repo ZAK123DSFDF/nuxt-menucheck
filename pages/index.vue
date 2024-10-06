87c7d7d70276bc91b2fea23157382b4cf74085f7
ZAK123DSFDF <zekariyasberihun281@gmail.com>
2024-10-06 19:52:14 +0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const { data } = await useFetch(
+  "https://fullstack-food-order.onrender.com/menu/all"
+);
+onMounted(() => {
+  console.log(
+    data?.value.map((value) => {
+      return value.Picture;
+    })
+  );
+});
+</script>
+
+<template>
+  <div>this is the home</div>
+  <div v-for="(data, index) in data">
+    <NuxtImg :src="data.Picture[0]" width="100" height="100" alt="menu item" />
+  </div>
+</template>
